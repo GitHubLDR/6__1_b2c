ec25d4ac58d77f14039f5c2977c90e2f23f2bb36
Devendra <devndrareddyinfo@gmail.com>
2017-01-20 08:13:46 +0530

    loading  DATA through initial setup from ldrinitialdata

--- ldr/ldrinitialdata/src/com/ldr/initialdata/setup/InitialDataSystemSetup.java
+++ ldr/ldrinitialdata/src/com/ldr/initialdata/setup/InitialDataSystemSetup.java
@@ -9,31 +9,36 @@
  * Information and shall use it only in accordance with the terms of the
  * license agreement you entered into with hybris.
  *
- *  
+ *
  */
 package com.ldr.initialdata.setup;
 
 import de.hybris.platform.commerceservices.dataimport.impl.CoreDataImportService;
 import de.hybris.platform.commerceservices.dataimport.impl.SampleDataImportService;
 import de.hybris.platform.commerceservices.setup.AbstractSystemSetup;
+import de.hybris.platform.commerceservices.setup.data.ImportData;
+import de.hybris.platform.commerceservices.setup.events.CoreDataImportedEvent;
+import de.hybris.platform.commerceservices.setup.events.SampleDataImportedEvent;
 import de.hybris.platform.core.initialization.SystemSetup;
 import de.hybris.platform.core.initialization.SystemSetup.Process;
 import de.hybris.platform.core.initialization.SystemSetup.Type;
 import de.hybris.platform.core.initialization.SystemSetupContext;
 import de.hybris.platform.core.initialization.SystemSetupParameter;
 import de.hybris.platform.core.initialization.SystemSetupParameterMethod;
-import com.ldr.initialdata.constants.LdrInitialDataConstants;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Required;
 
+import com.ldr.initialdata.constants.LdrInitialDataConstants;
+
 
 /**
  * This class provides hooks into the system's initialization and update processes.
- * 
+ *
  * @see "https://wiki.hybris.com/display/release4/Hooks+for+Initialization+and+Update+Process"
  */
 @SystemSetup(extension = LdrInitialDataConstants.EXTENSIONNAME)
@@ -41,6 +46,10 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 {
 	@SuppressWarnings("unused")
 	private static final Logger LOG = Logger.getLogger(InitialDataSystemSetup.class);
+
+	public static final String APPAREL = "ldrApparel";
+	public static final String APPAREL_UK = "ldrApparel-uk";
+	public static final String APPAREL_DE = "ldrApparel-de";
 
 	private static final String IMPORT_CORE_DATA = "importCoreData";
 	private static final String IMPORT_SAMPLE_DATA = "importSampleData";
@@ -69,7 +78,7 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 	/**
 	 * Implement this method to create initial objects. This method will be called by system creator during
 	 * initialization and system update. Be sure that this method can be called repeatedly.
-	 * 
+	 *
 	 * @param context
 	 *           the context provides the selected parameters and values
 	 */
@@ -109,6 +118,22 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 		/*
 		 * Add import data for each site you have configured
 		 */
+
+
+		final List<ImportData> importData = new ArrayList<ImportData>();
+
+		final ImportData apparelImportData = new ImportData();
+		apparelImportData.setProductCatalogName(APPAREL);
+		apparelImportData.setContentCatalogNames(Arrays.asList(APPAREL_UK, APPAREL_DE));
+		apparelImportData.setStoreNames(Arrays.asList(APPAREL_UK, APPAREL_DE));
+		importData.add(apparelImportData);
+
+		getCoreDataImportService().execute(this, context, importData);
+		getEventService().publishEvent(new CoreDataImportedEvent(context, importData));
+
+		getSampleDataImportService().execute(this, context, importData);
+		getEventService().publishEvent(new SampleDataImportedEvent(context, importData));
+
 	}
 
 	public CoreDataImportService getCoreDataImportService()
