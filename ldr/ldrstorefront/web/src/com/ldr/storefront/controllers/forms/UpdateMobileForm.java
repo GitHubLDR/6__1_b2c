5499e131b28c49f37ed7ea47c1cf189b09b244b3
Devendra <devndrareddyinfo@gmail.com>
2017-01-20 09:51:47 +0530

    commmiting missing files

---
+++ ldr/ldrstorefront/web/src/com/ldr/storefront/controllers/forms/UpdateMobileForm.java
@@ -0,0 +1,69 @@
+/**
+ *
+ */
+package com.ldr.storefront.controllers.forms;
+
+/**
+ * @author lmr_pad
+ *
+ */
+public class UpdateMobileForm
+{
+	private String mobile;
+	private String chkMobile;
+	private String password;
+
+	/**
+	 * @return the mobile
+	 */
+	public String getMobile()
+	{
+		return mobile;
+	}
+
+	/**
+	 * @param mobile
+	 *           the mobile to set
+	 */
+	public void setMobile(final String mobile)
+	{
+		this.mobile = mobile;
+	}
+
+	/**
+	 * @return the chkMobile
+	 */
+	public String getChkMobile()
+	{
+		return chkMobile;
+	}
+
+	/**
+	 * @param chkMobile
+	 *           the chkMobile to set
+	 */
+	public void setChkMobile(final String chkMobile)
+	{
+		this.chkMobile = chkMobile;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword()
+	{
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *           the password to set
+	 */
+	public void setPassword(final String password)
+	{
+		this.password = password;
+	}
+
+
+
+}
